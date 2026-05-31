2a89b5a54e0312aae92910297d555565e91c6036
Trinity <zborovskyart@gmail.com>
2026-05-31 12:33:25 +0300

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,21 @@
 use solana_sdk::pubkey::Pubkey;
 
 pub struct AssetConfig {
+    pub loan_amount: u64,
     pub reserve: Pubkey,
     pub liquidity_supply: Pubkey,
     pub liquidity_mint: Pubkey,
     pub is_sol: bool,
+}
+
+pub struct ProtocolConfig {
+    pub program_id: Pubkey,
+    pub lending_market: Pubkey,
+    pub vault: Pubkey,
+    pub fee_receiver: Pubkey
+}
+
+pub struct User {
+    pub rpc_url: String,
+    pub private_key: String
 }
