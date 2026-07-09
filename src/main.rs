c28b3d4ea70838a9868454c1f3f5cb473a577ee5
Trinity <zborovskyart@gmail.com>
2026-07-09 21:36:46 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,47 @@
 mod types;
 mod flashloan;
+mod meteora;
 
 use types::*;
-
-use flashloan::execute_flash_loan;
+use flashloan::execute;
+use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use solana_commitment_config::CommitmentConfig;
+use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer};
 use std::str::FromStr;
-use solana_sdk::pubkey::Pubkey;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
+
     let user: User = User {
         rpc_url: std::env::var("RPC_URL")?,
-        private_key: std::env::var("PRIVATE_KEY")?
+        private_key: std::env::var("PRIVATE_KEY")?,
     };
 
-    let protocol_config: ProtocolConfig = ProtocolConfig { 
-        program_id: Pubkey::from_str(types::PROGRAM_ID_STR)?, 
-        lending_market: Pubkey::from_str(types::LENDING_MARKET_STR)?, 
-        vault: Pubkey::from_str(types::VAULT_STR)?, 
-        fee_receiver: Pubkey::from_str(types::FEE_RECEIVER)? 
+    let protocol_config: ProtocolConfig = ProtocolConfig {
+        program_id: Pubkey::from_str(PROGRAM_ID_STR)?,
+        lending_market: Pubkey::from_str(LENDING_MARKET_STR)?,
+        vault: Pubkey::from_str(VAULT_STR)?,
+        fee_receiver: Pubkey::from_str(FEE_RECEIVER)?,
     };
 
     let asset: AssetConfig = AssetConfig {
-        loan_amount: 1_000_000,
-        reserve: Pubkey::from_str(types::SOL_RESERVE)?,
-        liquidity_supply: Pubkey::from_str(types::SOL_LIQUIDITY_SUPPLY)?,
-        liquidity_mint: Pubkey::from_str(types::SOL_LIQUIDITY_MINT)?,
-        is_sol: true,
+        loan_amount: 1_000_0000, // 0.1 SOL
+        reserve: Pubkey::from_str(SOL_RESERVE)?,
+        liquidity_supply: Pubkey::from_str(SOL_LIQUIDITY_SUPPLY)?,
+        liquidity_mint: Pubkey::from_str(SOL_LIQUIDITY_MINT)?,
     };
 
-    let pools: SwapPools = SwapPools {
-        orca_pool: types::ORCA_SOL_USDC_MARKET.to_string()
-    };
+    let client: RpcClient = RpcClient::new_with_commitment(user.rpc_url, CommitmentConfig::confirmed());
+    let payer: Keypair = Keypair::from_base58_string(&user.private_key);
+    println!("Wallet: {}", payer.pubkey());
 
-    match execute_flash_loan(user, protocol_config, asset, pools).await {
-        Ok(_) => {},
-        Err(e) => eprintln!("process crashed with error: {:?}", e),
-    }
+    let alt_address = Pubkey::from_str(&std::env::var("ALT_ADDRESS")?)?;
 
-    Ok(())
+    let route = vec![
+        Dex::Orca { pool: Pubkey::from_str(ORCA_SOL_USDC_MARKET)? },
+        Dex::MeteoraDlmm { lb_pair: Pubkey::from_str("BGm1tav58oGcsQJehL9WXBFXF7D27vZsKefj4xJKD5Y")? },
+    ];
+
+    execute(&client, &payer, &protocol_config, &asset, alt_address, &route).await
 }
